82917ca777a39733653c31689da8f4c405716bfe
Robert Forster <robert.forster@beamvex.co.uk>
2025-05-13 09:08:34 +0100

    Update rust-analyzer settings and add multi-peer POC implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
 use std::sync::Arc;
+use tokio::task::JoinError;
 use tracing::info;
 
 use subversive::types::{args::Args, config::Config, state::AppState};
@@ -35,14 +36,51 @@ async fn main() -> Result<()> {
 }
 
 #[cfg(feature = "poc")]
+async fn run_poc(
+    name: &str,
+    port: u16,
+) -> Result<tokio::task::JoinHandle<Result<Result<(), anyhow::Error>, JoinError>>> {
+    let mut config = Config::load().await;
+    config.database = Some(format!("{}.db", name));
+    config.port = Some(port);
+    let db = Arc::new(
+        DbContext::new(
+            config
+                .database
+                .clone()
+                .unwrap_or("subversive.db".to_string()),
+        )
+        .await?,
+    );
+
+    let app_state = Arc::new(AppState {
+        peers: Default::default(),
+        db,
+        actual_port: port,
+        config: config.clone(),
+        own_address: format!("https://localhost:{}", port),
+    });
+
+    let server_handle = tokio::spawn(subversive_server::server::spawn_server(app_state));
+    Ok(server_handle)
+}
+
+#[cfg(feature = "poc")]
 #[tokio::main]
 async fn main() -> Result<()> {
     update_tracing("info");
     subversive_utils::tui_utils::banner();
-    let args = Args::parse();
-    let port = args.port.unwrap_or(8080);
 
-    info!("Starting subversive poc on port {}", port);
+    info!("Starting subversive poc going to run multiple peers at once to test the network");
+
+    let mut handles = vec![];
+    for i in 8080..8090 {
+        handles.push(run_poc(&format!("peer_{}", i), i).await?);
+    }
+
+    info!("Press Ctrl+C to exit");
+
+    tokio::signal::ctrl_c().await?;
 
     Ok(())
 }
