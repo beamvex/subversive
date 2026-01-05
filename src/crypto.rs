2cbff0664efe8811e61d57568dabaab89d3c6978
Robert Forster <robert.forster@beamvex.co.uk>
2026-01-05 19:26:16 +0000

    feat: add sign module and export sign function from crypto module

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,8 @@
 #[path = "crypto/generate.rs"]
 mod generate;
 
+#[path = "crypto/sign.rs"]
+mod sign;
 
 pub use generate::generate_key;
+pub use sign::sign;
---
+++ src/crypto/sign.rs
@@ -0,0 +1,37 @@
+use ed25519_dalek::SigningKey;
+use ed25519_dalek::Signer;
+
+pub fn sign(data: &[u8], private_key: &[u8]) -> Vec<u8> {
+    let private_key: &[u8; 32] = private_key
+        .try_into()
+        .expect("ed25519 private key must be 32 bytes");
+    let signing_key = SigningKey::from_bytes(private_key);
+    let signature = signing_key.sign(data);
+    signature.to_bytes().to_vec()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::utils::bytes_to_base36;
+    use crate::crypto::generate_key;
+
+    #[test]
+    fn test_sign() {
+        let (private_key, public_key) = generate_key();
+
+        let data = b"test";
+        let signature = sign(data, &private_key);
+
+        println!("1. private_key_b36: {}", bytes_to_base36(&private_key));
+        println!("2. public_key_b36: {}", bytes_to_base36(&public_key));
+        println!("3. data: {}", bytes_to_base36(data));
+        println!("4. signature: {}", bytes_to_base36(&signature));
+
+        assert_eq!(private_key.len(), 32);
+        assert_eq!(public_key.len(), 32);
+        assert_eq!(data.len(), 4);
+        assert_eq!(signature.len(), 64);
+    }
+}
