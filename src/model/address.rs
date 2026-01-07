35db27b78f829f4245fd76fdcac96073c9a90f6f
Robert Forster <robert.forster@beamvex.co.uk>
2026-01-07 19:23:41 +0000

    feat: add zerocopy dependencies and implement IntoBytes for Address struct
    Add zerocopy and zerocopy-derive crate dependencies. Derive Immutable and IntoBytes traits for Address struct with repr(C) layout. Add test to verify Address can be converted to bytes and encoded as base36.

--- src/model/address.rs
+++ src/model/address.rs
@@ -1,3 +1,26 @@
+use zerocopy::{Immutable, IntoBytes};
+
+#[repr(C)]
+#[derive(Debug, Default, Immutable, IntoBytes)]
 pub struct Address {
     public_key: Vec<u8>,
+}
+
+mod tests {
+    use super::*;
+    use crate::utils::{base36_to_bytes_32, bytes_to_base36};
+
+    use zerocopy::{Immutable, IntoBytes};
+
+    #[test]
+    fn test_address() {
+        let private_key_bytes = base36_to_bytes_32("3375t72oexdn8n814mi1z8yjpubm9yy1uxz1f9o1hpz0qye833");
+
+        let address = Address{public_key: private_key_bytes.to_vec()};
+        
+        let address_bytes = address.as_bytes();
+        
+        println!("address_bytes: {}", bytes_to_base36(&address_bytes));
+    }
+
 }
