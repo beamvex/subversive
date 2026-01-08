828b74bbfd6c0f82a5507f4b609af81a67e4ba42
Robert Forster <robert.forster@beamvex.co.uk>
2026-01-08 07:24:36 +0000

    feat: change Address public_key from Vec to fixed 32-byte array
    Replace Vec<u8> with [u8; 32] for Address public_key field. Update zerocopy derives from Immutable/IntoBytes to FromZeroes/FromBytes/AsBytes/Unaligned. Update test to convert Vec to fixed array using try_into with error handling.

--- src/model/address.rs
+++ src/model/address.rs
@@ -1,20 +1,26 @@
-use zerocopy::{Immutable, IntoBytes};
+use zerocopy::{AsBytes, FromBytes, FromZeroes, Unaligned};
 
 #[repr(C)]
-#[derive(Debug, Default, Immutable, IntoBytes)]
+#[derive(Debug, Default, FromZeroes, FromBytes, AsBytes, Unaligned)]
 pub struct Address {
-    public_key: Vec<u8>,
+    public_key: [u8; 32],
 }
 
 mod tests {
     use super::*;
+    use zerocopy::AsBytes;
     use crate::utils::{base36_to_bytes_32, bytes_to_base36};
 
     #[test]
     fn test_address() {
         let private_key_bytes = base36_to_bytes_32("3375t72oexdn8n814mi1z8yjpubm9yy1uxz1f9o1hpz0qye833");
 
-        let address = Address{public_key: private_key_bytes.to_vec()};
+        let public_key: [u8; 32] = private_key_bytes
+            .as_slice()
+            .try_into()
+            .expect("base36_to_bytes_32 must return 32 bytes");
+
+        let address = Address { public_key: public_key };
         
         let address_bytes = address.as_bytes();
         
