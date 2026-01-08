d8d96101bafe6ffa0a986513807be316eccfcf6d
Robert Forster <robert.forster@beamvex.co.uk>
2026-01-08 07:52:01 +0000

    feat: change Signature from Vec to fixed 64-byte array and add zerocopy support
    Replace Vec<u8> with [u8; 64] for Signature field. Add zerocopy derives (FromZeroes, FromBytes, AsBytes, Unaligned) with repr(C) layout. Implement Default trait. Add test to verify signature creation from private key and conversion to base36 format.

--- src/model/signature.rs
+++ src/model/signature.rs
@@ -1,3 +1,40 @@
+use zerocopy::{AsBytes, FromBytes, FromZeroes, Unaligned};
+
+#[repr(C)]
+#[derive(Debug, FromZeroes, FromBytes, AsBytes, Unaligned)]
 pub struct Signature {
-    pub signature: Vec<u8>,
+    pub signature: [u8; 64],
 }
+
+impl Default for Signature {
+    fn default() -> Self {
+        Self { signature: [0u8; 64] }
+    }
+}
+
+mod tests {
+    use super::*;
+    use zerocopy::AsBytes;
+    use crate::crypto::sign;
+    use crate::utils::{base36_to_bytes_32, bytes_to_base36};
+
+    #[test]
+    fn test_signature() {
+        let private_key_bytes = base36_to_bytes_32("3375t72oexdn8n814mi1z8yjpubm9yy1uxz1f9o1hpz0qye833");
+
+        let data = b"test";
+        let signature_bytes = sign(data, &private_key_bytes);
+        let signature: [u8; 64] = signature_bytes
+            .as_slice()
+            .try_into()
+            .expect("ed25519 signature must be 64 bytes");
+
+        let signature = Signature { signature };
+        
+        let signature_bytes = signature.as_bytes();
+        
+        println!("signature_bytes: {}", bytes_to_base36(&signature_bytes));
+    }
+
+}
+
