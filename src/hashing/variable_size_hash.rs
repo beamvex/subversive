db87a7f37fd65fceeb2cd94f77493fd8e28059cf
Robert Forster <robert.forster@beamvex.co.uk>
2026-01-29 13:42:29 +0000

    feat: add hashable_by_type and impl_hash_by_type macros with multi-hash support and tests
    Add hashable_by_type macro that wraps impl_hash_by_type for single hash type implementations. Add impl_hash_by_type macro that generates hash method with custom function name and hash type parameters. Update hashable macro to use impl_hash_by_type for both Hash and Hash384 variants with hash() and hash384() method names. Move impl_verify macro definition after impl_hash_by_type. Add test_hash() function testing

--- src/hashing/variable_size_hash.rs
+++ src/hashing/variable_size_hash.rs
@@ -55,3 +55,71 @@ macro_rules! variable_size_hash {
 
 variable_size_hash!(Hash, 32);
 variable_size_hash!(Hash384, 48);
+
+#[macro_export]
+macro_rules! hashable {
+    ($t:ty) => {
+        $crate::impl_hash_by_type!($t, $crate::hashing::Hash, hash);
+        $crate::impl_hash_by_type!($t, $crate::hashing::Hash384, hash384);
+        $crate::impl_verify!($t);
+    };
+}
+
+#[macro_export]
+macro_rules! hashable_by_type {
+    ($t:ty, $h:ty, $fn_name:ident) => {
+        $crate::impl_hash_by_type!($t, $h, $fn_name);
+    };
+}
+
+#[macro_export]
+macro_rules! impl_hash_by_type {
+    ($t:ty, $h:ty, $fn_name:ident) => {
+        impl $t {
+            pub fn $fn_name(&self, hash_algorithm: $crate::hashing::HashAlgorithm) -> $h {
+                <$h>::hash_bytes(self.as_bytes(), hash_algorithm)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_verify {
+    ($t:ty) => {
+        impl $t {
+            pub fn verify(&self, _hash: &$crate::hashing::Hash) -> bool {
+                unimplemented!()
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::serialise::SerialString;
+    use crate::serialise::SerialiseType;
+
+    #[test]
+    fn test_hash() {
+        let bytes: Vec<u8> = vec![1, 2, 3];
+        let hash = Hash::hash_bytes(&bytes, HashAlgorithm::KECCAK256);
+
+        let hash_str: SerialString = hash.into_serial_string(SerialiseType::Base36);
+        println!("hash: {}", hash_str.get_string());
+        println!("hash debug: {:?}", hash);
+
+        let hash = Hash::hash_bytes(&bytes, HashAlgorithm::SHA256);
+
+        let hash_str: SerialString = hash.into_serial_string(SerialiseType::Base36);
+        println!("hash: {}", hash_str.get_string());
+        println!("hash debug: {:?}", hash);
+
+        let hash = Hash384::hash_bytes(&bytes, HashAlgorithm::KECCAK384);
+
+        let hash_str: SerialString = hash.into_serial_string(SerialiseType::Base36);
+        println!("hash: {}", hash_str.get_string());
+        println!("hash debug: {:?}", hash);
+    }
+}
