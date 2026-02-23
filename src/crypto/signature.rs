b36ab9cffd260d464dc6e15e7a30ea99600704a5
Robert Forster <robert.forster@beamvex.co.uk>
2026-02-23 08:29:10 +0000

    refactor: add digest crate dependency and implement custom TestDigest for signature testing
    Add digest 0.10 dependency to Cargo.toml. Implement TestDigest struct with Digest trait for testing purposes in signature module. Change sha2::Digest import to digest::Digest. Add digest::generic_array import. Extend test data length in test_signature

--- src/crypto/signature.rs
+++ src/crypto/signature.rs
@@ -80,18 +80,77 @@ mod tests {
 
     use base_xx::EncodedString;
     use base_xx::Encoding;
+    use digest::generic_array;
     use ed25519_dalek::Signer;
     use ed25519_dalek::SigningKey;
     use rand_core::OsRng;
 
-    use sha2::Digest;
+    use digest::Digest;
     use sha3::Keccak512;
     use slogger::debug;
 
     #[test]
     fn test_signature() {
+        #[derive(Debug, Default)]
+        struct TestDigest {
+            data: Vec<u8>,
+        }
+
+        impl Digest for TestDigest {
+            fn output_size() -> usize {
+                64
+            }
+
+            fn new() -> Self {
+                Self::default()
+            }
+
+            fn new_with_prefix(data: impl AsRef<[u8]>) -> Self {
+                Self {
+                    data: data.as_ref().to_vec(),
+                }
+            }
+
+            fn update(&mut self, data: impl AsRef<[u8]>) {
+                self.data.extend_from_slice(data.as_ref());
+            }
+
+            fn chain_update(self, data: impl AsRef<[u8]>) -> Self {
+                Self {
+                    data: self.data.clone(),
+                }
+            }
+
+            fn finalize(self) -> digest::generic_array::GenericArray<u8, _> {
+                self.data
+            }
+
+            fn finalize_into(self, out: &mut digest::generic_array::GenericArray<u8, _>) {
+                out.copy_from_slice(&self.data);
+            }
+
+            fn finalize_reset(&mut self) -> digest::generic_array::GenericArray<u8, _> {
+                todo!()
+            }
+
+            fn finalize_into_reset(
+                &mut self,
+                out: &mut digest::generic_array::GenericArray<u8, _>,
+            ) {
+                todo!()
+            }
+
+            fn reset(&mut self) {
+                todo!()
+            }
+
+            fn digest(data: impl AsRef<[u8]>) -> digest::Output<Self> {
+                todo!()
+            }
+        }
+
         let signing_key = SigningKey::generate(&mut OsRng);
-        let data = b"test big long data test test big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data test";
+        let data = b"test big long data test test big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data testtest big long data test";
 
         let mut digest = Keccak512::new();
         digest.update(data);
